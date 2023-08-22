6d14033004371351a205f79992aaacb5cd8bc657
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-22 16:33:44 +0530

    Create Modal.js

---
+++ components/Modal.js
@@ -0,0 +1,32 @@
+import { useAuth } from '@/context/AuthContext'
+import React, { useState, useEffect } from 'react'
+import ReactDom from 'react-dom'
+
+
+export default function Modal(props) {
+    const { setOpenModal } = props
+    const [_document, set_document] = useState(null)
+    const { logout } = useAuth()
+
+    useEffect(() => {
+        set_document(document)
+    }, [])
+
+    if (!_document){ return null }
+
+    return ReactDom.createPortal(
+        <div className='fixed w-screen w-full h-screen top-0 left-0 text-lg sm:text-xl bg-white text-slate-900 flex flex-col'>
+            <div className='flex items-center justify-between border-b border-solid border-slate-900 p-4'>
+                <h1 className='text-3xl font-extrabold select-none sm:text-6xl'>MENU</h1>
+                <i onClick={() => setOpenModal(false)} className="fa-solid fa-xmark duration-300 hover:rotate-90 cursor-pointer text-2xl sm:text-3xl"></i>
+            </div>
+            <div className='p-4 flex flex-col gap-3'>
+                <h2 onClick = {() => {
+                    logout()
+                    setOpenModal(false)
+                }} className='select-none duration-300 hover:pl-2 cursor-pointer'>Logout</h2>
+            </div>
+        </div>,
+        _document.getElementById('portal')
+    )
+}
