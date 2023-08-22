bc38a692fef4bfbb3cb0275925e0c4a1776f3349
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-22 21:37:50 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
             <a href ='https://www.instagram.com/sidlakhani_/'>
                 <i className="fa-brands fa-instagram duration-300 hover:opacity-30 cursor-pointer"></i>
             </a>
-            <a href ='https://www.linkedin.com/in/siddhesh-lakhani-0aa228257/'>
+            <a href ='https://www.linkedin.com/in/siddhesh-lakhani'>
                 <i className="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer"></i>
             </a>
             <a href='https://github.com/sid-lakhani'>
