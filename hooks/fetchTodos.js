4d3d03fbbc4c0f17914c12882f5f3b7d477b891a
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-24 18:13:34 +0530

    Update fetchTodos.js

--- hooks/fetchTodos.js
+++ hooks/fetchTodos.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { doc, getDoc } from 'firebase/firestore'
 
-export default function fetchTodos() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function useFetchTodos() {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const [todos, setTodos] = useState(null)
+
+    const { currentUser } = useAuth()
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const docRef = doc(db, "users", currentUser.uid)
+                const docSnap = await getDoc(docRef)
+                if (docSnap.exists()) {
+                    console.log(docSnap.data())
+                }
+                if (docSnap.exists) {
+                    console.log("oopsie")
+                }
+            } catch (err) {
+                setError("Failed to load Todos")
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [])
+
+    return { loading, error, todos }
 }
