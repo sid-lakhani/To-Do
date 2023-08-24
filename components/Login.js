6170069f66b6faa574ee570d853a7a74ff6863c9
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-24 14:33:59 +0530

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -40,7 +40,7 @@ export default function Login() {
             <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder='Email Address' onKeyDown= {handleKeyDown} className='outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 select-none p-2 w-full max-w-[40ch]' />
             <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Password' onKeyDown={handleKeyDown} className='outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 select-none p-2 w-full max-w-[40ch]' />
             <button onClick={submitHandler} className='w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900'>
-                <h2 className='relative z-20 '>SUBMIT</h2>
+                <h2 className='relative z-20 select-none '>SUBMIT</h2>
             </button>
             <h2 onClick={() => setIsLoggingIn(!isLoggingIn)} className='select-none duration-300 hover:scale-110 hover:opacity-50 cursor-pointer'> {!isLoggingIn ? 'Login' : 'Register'} </h2>
         </div>
