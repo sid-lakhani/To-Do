2b5e47eab6e0461944aa0c06e86c22640bd2ac73
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-26 09:14:26 +0530

    Update TodoCard.js

--- components/TodoCard.js
+++ components/TodoCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 export default function TodoCard(props) {
-    const { children, edit, setEdit, edittedValue, setEdittedValue } = props
+    const { children, edit, handleAddEdit, edittedValue, setEdittedValue, todoKey } = props
     return (
         <div className='p-2 relative sm:p-3 border flex items-stretch border-white-300 border-solid'>
             <div className='flex-1 capitalize'>
-                {!edit ? <>{children}</>: (
+                {!(edit === todoKey) ? <>{children}</>: (
                     <input className='bg-inherit text-white-300 p-2 outline-none' value={edittedValue} onChange={(e) => setEdittedValue(e.target.value)}/>
                 )}
             </div>
             <div className='flex items-center'>
-                <i onClick={() => setEdit(true)} className="fa-solid fa-pen-to-square px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>
+                <i onClick={handleAddEdit(todoKey)} className="fa-solid fa-pen-to-square px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>
                 <i className="fa-solid fa-trash px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>
             </div>
         </div>
