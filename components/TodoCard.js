2bf4eddc6ae7ceb6474beb37a988b4d9f96fec49
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-26 23:29:06 +0530

    Update TodoCard.js

--- components/TodoCard.js
+++ components/TodoCard.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 export default function TodoCard(props) {
-    const { children, edit, handleAddEdit, edittedValue, setEdittedValue, todoKey, handleEditTodo, handleDelete } = props
+    const { 
+        children, 
+        edit, 
+        handleAddEdit, 
+        edittedValue, 
+        setEdittedValue, 
+        todoKey, 
+        handleEditTodo, 
+        handleDelete, 
+        handleCompleteTodo, 
+        isCompletedTodo
+    } = props
 
     return (
         <div className='p-2 relative sm:p-3 border flex items-stretch border-white-300 border-solid'>
@@ -11,8 +22,9 @@ export default function TodoCard(props) {
                 )}
             </div>
             <div className='flex items-center'>
-                {(edit === todoKey) ? <i onClick={handleEditTodo} className="fa-solid fa-check px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i> : <i onClick={handleAddEdit(todoKey)} className="fa-solid fa-pen-to-square px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>}
-                <i onClick={handleDelete(todoKey)} className="fa-solid fa-trash px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>
+                {isCompletedTodo ? null : ((edit === todoKey) ? (<i onClick={handleEditTodo} className="fa-solid fa-check px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>) : (<i onClick={handleAddEdit(todoKey)} className="fa-solid fa-pen-to-square px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>))}
+                {isCompletedTodo ? null : <i onClick={handleCompleteTodo} className="fa-solid fa-square-check px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>}
+                {isCompletedTodo ? null : <i onClick={handleDelete(todoKey)} className="fa-solid fa-trash px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>}
             </div>
         </div>
     )
