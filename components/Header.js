99a5992d05d9134ac04a10edcdebe311496198e7
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-26 10:22:25 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
     return (
         <>
             {openModal && <Modal setOpenModal={setOpenModal}/>}
-            <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white shadow'>
+            <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
                 <h1 className='text-3xl font-extrabold select-none sm:text-6xl'>TODO-LIST</h1>
                 {currentUser && <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-2xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>}
                 {!currentUser && <i className="fa-solid fa-check text-2xl duration-300 hover: sm:text-3xl"></i>}
