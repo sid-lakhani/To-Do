f37d3005b1372f6089d9dba86b9104788498d64a
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-26 09:38:39 +0530

    Update TodoCard.js

--- components/TodoCard.js
+++ components/TodoCard.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 export default function TodoCard(props) {
-    const { children, edit, handleAddEdit, edittedValue, setEdittedValue, todoKey } = props
+    const { children, edit, handleAddEdit, edittedValue, setEdittedValue, todoKey, handleEditTodo } = props
 
     return (
         <div className='p-2 relative sm:p-3 border flex items-stretch border-white-300 border-solid'>
-            <div className='flex-1 capitalize'>
+            <div className='flex-1 flex capitalize'>
                 {!(edit === todoKey) ? <>{children}</>: (
-                    <input className='bg-inherit text-white-300 capitalize' value={edittedValue} onChange={(e) => setEdittedValue(e.target.value)}/>
+                    <input className='bg-inherit flex-1 text-white-300 outline-none capitalize' value={edittedValue} onChange={(e) => setEdittedValue(e.target.value)}/>
                 )}
             </div>
             <div className='flex items-center'>
-                {(edit === todoKey) ? <i className="fa-solid fa-check px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i> : <i onClick={handleAddEdit(todoKey)} className="fa-solid fa-pen-to-square px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>}
+                {(edit === todoKey) ? <i onClick={handleEditTodo} className="fa-solid fa-check px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i> : <i onClick={handleAddEdit(todoKey)} className="fa-solid fa-pen-to-square px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>}
                 <i className="fa-solid fa-trash px-2 duration-300 hover:opacity-50 hover:scale-125 cursor-pointer"></i>
             </div>
         </div>
