1a4915a798d2e632cfac849b39b795bbf916c7cd
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-27 11:30:42 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className='flex justify-center items-center gap-5 py-3'>
+        <div className='fixed bottom-0 w-full left-0 bg-inherit flex items-center justify-center p-2 border-t border-solid border-white'>
             <a href ='https://www.instagram.com/sidlakhani_/'>
-                <i className="fa-brands fa-instagram duration-300 hover:opacity-30 cursor-pointer"></i>
+                <i className="fa-brands fa-instagram text-xl duration-300 hover:opacity-30 cursor-pointer px-2 sm:px-4"></i>
             </a>
             <a href ='https://www.linkedin.com/in/siddhesh-lakhani'>
-                <i className="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer"></i>
+                <i className="fa-brands fa-linkedin text-xl duration-300 hover:opacity-30 cursor-pointer px-2 sm:px-4"></i>
             </a>
             <a href='https://github.com/sid-lakhani'>
-                <i className="fa-brands fa-github duration-300 hover:opacity-30 cursor-pointer"></i>
+                <i className="fa-brands fa-github text-xl duration-300 hover:opacity-30 cursor-pointer px-2 sm:px-4"></i>
             </a>
         </div>
     )
