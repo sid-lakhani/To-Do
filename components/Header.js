5ca2d9c7b5fdc117952cfcafa4b48ac75de3e524
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-27 11:48:19 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
             <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between px-5 sm:px-10 border-b border-solid border-white'>
                 <h1 className='text-6xl font-extrabold select-none py-2'>TO-DO</h1>
                 {currentUser && <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-4xl duration-300 hover:opacity-40 cursor-pointer"></i>}
-                {!currentUser && <i className="fa-solid fa-check text-4xl duration-300 hover: "></i>}
+                {!currentUser && <i className="fa-solid fa-check text-4xl"></i>}
             </div>
         </>
     )
