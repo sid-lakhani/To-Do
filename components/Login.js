a2b743f8dfc46d6a30fb690a39082c4516ac0394
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-08-28 22:08:40 +0530

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -35,7 +35,7 @@ export default function Login() {
     return (
         <div className='fixed inset-0 flex items-center justify-center bg-inherit overflow-hidden'>
             <div className='flex-1 flex flex-col text-xm sm:text-sm justify-center items-center gap-1 p-4'>
-                <h1 className='font-extrabold select-none text-3xl uppercase underline'>
+                <h1 className='font-extrabold select-none text-3xl pb-1 uppercase underline'>
                     {isLoggingIn ? 'Login' : 'Register'}
                 </h1>
                 {error && <div className='w-full max-w-[40ch] border-rose-300 border text-center select-none border-solid text-rose-400 py-2'>{error}</div>}
@@ -60,7 +60,7 @@ export default function Login() {
                 <button onClick={submitHandler} className='w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900'>
                     <h2 className='relative z-20 select-none'>SUBMIT</h2>
                 </button>
-                <h2 className='select-none underline capitalize'>{isLoggingIn ? 'Dont have an account?' : 'Have an account already?'}</h2>
+                <h2 className='select-none pt-1 underline capitalize'>{isLoggingIn ? 'Dont have an account?' : 'Have an account already?'}</h2>
                 <h2 onClick={() => setIsLoggingIn(!isLoggingIn)} className='select-none duration-300 hover:scale-110 hover:opacity-50 cursor-pointer '> {!isLoggingIn ? 'Login' : 'Register'} </h2>
             </div>
         </div>
